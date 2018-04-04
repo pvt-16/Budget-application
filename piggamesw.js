90611820cffe603ce06d9164a3c8cfe2eb9d8a17
pvt-16 <prabha0106@gmail.com>
2018-04-04 10:23:00 +0530

    Update piggamesw.js
    Testing notification close

--- piggamesw.js
+++ piggamesw.js
@@ -31,3 +31,10 @@ event.waitUntil(
   })
  );
 });
+
+self.addEventListener('notificationclose', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+
+  console.log('Closed notification: ' + primaryKey);
+});
