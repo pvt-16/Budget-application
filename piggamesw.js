7e6d81263167d118eeac18db669b1f75131800ea
pvt-16 <prabha0106@gmail.com>
2018-04-04 23:37:46 +0530

    Update piggamesw.js

--- piggamesw.js
+++ piggamesw.js
@@ -6,7 +6,7 @@ self.addEventListener('install', function(event) {
        cache.add('PigGame.js')
                           cache.add('PigGameCss.css')
                           cache.add('back.jpg')
-                          cache.add('../Pig-Game-Dice-Images/dice-5.png') 
+                          cache.add('./Pig-Game-Dice-Images/dice-5.png') 
       return;
     })
   );
